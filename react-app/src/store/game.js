27857d6e9d84a97a6f2ca11a9760bdbc89160097
JakeG97 <jgularteee@gmail.com>
2023-04-19 13:31:12 -0700

    games store/reducer created

---
+++ react-app/src/store/game.js
@@ -0,0 +1,42 @@
+const normalizer = (data) => {
+    const obj = {};
+    data.forEach((item) => {
+      obj[item.id] = item;
+    });
+    return obj;
+  };
+
+  const LOAD_GAMES = "games/LOAD_GAMES";
+
+  const loadGames = (allGameData) => ({
+    type: LOAD_GAMES,
+    payload: allGameData,
+  });
+
+
+  export const getAllGamesThunk = () => async (dispatch) => {
+    const response = await fetch(`/api/games`);
+  
+    if (response.ok) {
+      const allGameData = await response.json();
+      const normalizedGameData = normalizer(allGameData);
+      dispatch(loadWorkspaces(normalizedGameData));
+    }
+  };
+
+
+
+
+const initialState = {};
+
+const gamesReducer = (state = initialState, action) => {
+    let newState = { ...state };
+    switch (action.type) {
+        case LOAD_GAMES:
+          return { ...state, ...action.payload };
+        default:
+            return state;
+    }
+};
+
+export default gamesReducer;
