89f1db9dfbf355f6bed66b97fe9c155816c44e34
JakeG97 <jgularteee@gmail.com>
2023-04-19 14:31:53 -0700

    home page component basics started, also styling

---
+++ react-app/src/components/HomePage/index.js
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { login } from "../../store/session";
+import { getAllGamesThunk } from "../../store/game";
+import "./HomePage.css";
+import { useEffect } from "react";
+
+const HomePage = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  // Retrieve games from the store
+  const games = useSelector((state) => Object.values(state.games));
+
+  // Dispatch action to load all games when the component mounts
+  useEffect(() => {
+    dispatch(getAllGamesThunk());
+  }, [dispatch]);
+
+  const onClick = () => {
+    dispatch(login("demo@aa.io", "password"));
+    history.push("/");
+  };
+
+  return (
+    <>
+      <h1>SUMMIT</h1>
+      <div className="games-container">
+        {games.map((game) => (
+          <div key={game.id} className="game-card">
+            <img  className="games-list-image" src={game.image} alt={game.title} />
+            <h2 className="title-text">{game.title}</h2>
+            <p id="home-price"className="title-text">{game.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
