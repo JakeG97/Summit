67f5782db275fc8451840fa08d4b207e2e25cb3b
JakeG97 <jgularteee@gmail.com>
2023-04-24 14:59:44 -0700

    more styling for game details and reviews

--- react-app/src/components/Reviews/index.js
+++ react-app/src/components/Reviews/index.js
@@ -4,45 +4,52 @@ import { getAllReviewsThunk, deleteReviewThunk, updateReviewThunk } from "../../
 import './Reviews.css'
 
 const Review = ({ review }) => {
-    const dispatch = useDispatch();
-    const sessionUser = useSelector((state) => state.session.user);
-    const [showEditForm, setShowEditForm] = useState(false);
-    const [recommended, setRecommended] = useState(review.recommended);
-    const [description, setDescription] = useState(review.description);
-  
-    const handleDelete = () => {
-      dispatch(deleteReviewThunk(review.id));
-    };
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [recommended, setRecommended] = useState(review.recommended);
+  const [description, setDescription] = useState(review.description);
 
-    const handleEdit = (e) => {
-        e.preventDefault();
-        dispatch(updateReviewThunk(review.id, { recommended, description }));
-        setShowEditForm(false);
-      };
-  
-      return (
-        <div className="review">
-          <div className="review-container">
-            <img
-              className="profile-pic-review"
-              src={sessionUser.profile_picture}
-              alt="User profile picture"
-            />
+  const handleDelete = () => {
+    dispatch(deleteReviewThunk(review.id));
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    dispatch(updateReviewThunk(review.id, { recommended, description }));
+    setShowEditForm(false);
+  };
+
+  return (
+    <div className="review">
+      <div className="review-container">
+        <div className="profile-container">
+          <img
+            className="profile-pic-review-list"
+            src={sessionUser.profile_picture}
+            alt="User profile picture"
+          />
+          <p>{sessionUser.username}</p>
+        </div>
+        <div className="review-details">
+          <div className="review-info">
             {review.recommended ? (
               <p className="review-rating">
-                <i className="fas fa-thumbs-up"></i> Yes
+                <i id="thumb-up" className="fas fa-thumbs-up fa-flip-horizontal"></i> Recommended
               </p>
             ) : (
               <p className="review-rating">
-                <i className="fas fa-thumbs-down"></i> No
+                <i id="thumb-down" className="fas fa-thumbs-down fa-flip-horizontal"></i> Not Recommended
               </p>
             )}
-            <p className="review-content">{new Date(review.created_at).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</p>
-            <p className="review-content">{review.description}</p>
-            {review.reviewer_id === sessionUser && (
+            <p className="review-content">Posted: {new Date(review.created_at).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}</p>
+            <p className="review-description">{review.description}</p>
+            {review.reviewer_id === sessionUser.id && (
               <>
-                <button onClick={handleDelete}>Delete</button>
-                <button onClick={() => setShowEditForm(true)}>Edit</button>
+                <button className="review-buttons" onClick={handleDelete}>Delete</button>
+                <button className="review-buttons" onClick={() => setShowEditForm(true)}>
+                  <i className="fas fa-pen-square"></i> Edit Review
+                </button>
               </>
             )}
           </div>
@@ -50,7 +57,7 @@ const Review = ({ review }) => {
             <form onSubmit={handleEdit}>
               <div>
                 <label>
-                  Would you recommend this game?
+                  Do you still recommend this game?
                   <select
                     value={recommended}
                     onChange={(e) => setRecommended(e.target.value === "true")}
@@ -75,8 +82,10 @@ const Review = ({ review }) => {
             </form>
           )}
         </div>
-      );
-    };  
+      </div>
+    </div>
+  );  
+};  
 
 const Reviews = ({ gameId }) => {
   const dispatch = useDispatch();
@@ -88,7 +97,7 @@ const Reviews = ({ gameId }) => {
 
   return (
     <div className="reviews">
-      <h2>Reviews</h2>
+      <h2 className="review-list-title">MOST HELPFUL REVIEWS</h2>
       {reviews.length > 0 ? (
         reviews.map((review) => {
           if (review.game_id === Number(gameId)) {
