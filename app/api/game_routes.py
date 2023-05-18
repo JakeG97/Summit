ed6af234e295aa340c015ffa976a6b5418a8dfc6
JakeG97 <jgularteee@gmail.com>
2023-05-18 15:04:16 -0700

    Post game route created and working

--- app/api/game_routes.py
+++ app/api/game_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, redirect, request
 from app.models import User, Game, Review, db
 from flask_login import current_user, login_required
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from ..forms.game_form import GameForm
 
 
 game_routes = Blueprint('games', __name__)
@@ -24,3 +25,33 @@ def get_all_games():
 def get_single_game(game_id):
     game = Game.query.get(game_id)
     return game.to_dict()
+
+
+#TODO -----------  POST  --------------
+
+@game_routes.route('/create_game', methods=['POST'])
+@login_required
+def create_game():
+    form = GameForm()
+    form.csrf_token.data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        new_game = Game(
+            title=form.title.data,
+            image=form.image.data,
+            price=form.price.data,
+            release_date=form.release_date.data,
+            short_description=form.short_description.data,
+            full_description=form.full_description.data,
+            developer=form.developer.data,
+            publisher=form.publisher.data,
+            banner_image=form.banner_image.data,
+            other_images=form.other_images.data
+        )
+
+        db.session.add(new_game)
+        db.session.commit()
+
+        return new_game.to_dict()
+    else:
+        return {"errors": form.errors}, 400
