d8f236707fdb735e8364d5488289dc71b32f6bdb
JakeG97 <jgularteee@gmail.com>
2023-05-22 22:14:43 -0700

    edit game store setup

--- react-app/src/store/game.js
+++ react-app/src/store/game.js
@@ -10,6 +10,7 @@ const normalizer = (data) => {
   const LOAD_SINGLE_GAME = "games/LOAD_SINGLE_GAME";
   const CREATE_GAME = "games/CREATE_GAME";
   const DELETE_GAME = "games/DELETE_GAME";
+  const UPDATE_GAME = "games/UPDATE_GAME";
 
   const loadGames = (allGameData) => ({
     type: LOAD_GAMES,
@@ -29,14 +30,18 @@ const normalizer = (data) => {
   const deleteGame = (gameId) => ({
     type: DELETE_GAME,
     payload: gameId
-  })
+  });
+
+  const updateGame = (game) => ({
+    type: UPDATE_GAME,
+    payload: game,
+  });
 
   export const getAllGamesThunk = () => async (dispatch) => {
     const response = await fetch(`/api/games`);
   
     if (response.ok) {
       const allGameData = await response.json();
-      // console.log("HEY LOOK HERE", allGameData)
       const normalizedGameData = normalizer(allGameData);
       dispatch(loadGames(normalizedGameData));
     }
@@ -76,6 +81,21 @@ const normalizer = (data) => {
     }
   }
 
+  export const updateGameThunk = (gameId, gameData) => async (dispatch) => {
+    const res = await fetch(`/api/games/${gameId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(gameData),
+    });
+
+    if (res.ok) {
+      const game = await res.json();
+      dispatch(updateGame(game));
+    }
+  };
+
 const initialState = {};
 
 const gamesReducer = (state = initialState, action) => {
@@ -90,6 +110,8 @@ const gamesReducer = (state = initialState, action) => {
         case DELETE_GAME:
           delete newState[action.payload];
           return newState;
+        case UPDATE_GAME:
+          return { ...state, [action.payload.id]: action.payload, };
         default:
             return state;
     }
